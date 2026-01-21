e263b1d169c96a08923d3efd421e6f673a80b8a6
Nguyễn ngọc gia huy <nguyenngocgiahuy10102005@gmail.com>
2026-01-21 08:20:13 +0700

    ex2

---
+++ SLOT5/ex2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	float a, b, c;
+	printf("nhap a, b, c:");
+	scanf("%f%f%f", &a,&b,&c);
+	float max;
+	max = a;
+	if(max<b)
+		max = b;
+		if(max<c)
+			max = c;
+			printf("so lon nhat la: %.2f", max);
+}
